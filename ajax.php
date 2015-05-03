038fb2b0d2fbf4996e07b6760061186fe3012fed
Magnus Ekhall <magnus.ekhall@gmail.com>
2015-05-03 18:58:44 +0200

    Adjust prices via script on server instead of from browser.

--- ajax.php
+++ ajax.php
@@ -13,9 +13,6 @@ if (is_ajax()) {
         case "marquee":
             construct_marquee($return);
             break;
-        case "preis":
-            update_preis($return);
-            break;
         case "chart":
             construct_chart($_POST["brandNumber"], $return);
             break;
@@ -163,54 +160,6 @@ function construct_chart($brandNumber, $return) {
    echo json_encode($return);
    
    $conn->close();
-}
-
-function update_preis($return) {
-// To increment the Preis on all bier sorts.
-// They will increase the Preis varje time sidan ist refreshed.
-// Run a new loop, just for the sake of it ...
-    global $servername, $username, $password, $dbname;
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }     
-    
-    $sql = "SELECT * FROM `komponent`";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $preis = $row["Preis"];
-            $orig  = $row["Originalpreis"];
-            $mini  = $row["Minimipreis"];
-            $altpreis = $preis;
-            
-            // Algorithm to set the Preis - be creative...
-            $preis = $preis - ($preis-$mini)*0.05 + 0.2*rand(0, 2); 
-            
-            // We shouldn't loose money
-            if ($preis < $mini) {
-                $preis = $mini;
-            }	  
-            
-            // Set the new Preis and update the old Preis.
-            $sql = 'UPDATE `komponent` SET `Preis` = ' . $preis . ' WHERE `Brand`="' . $row["Brand"] . '"' ;
-            $conn->query($sql);
-            $sql = 'UPDATE `komponent` SET `Altpreis` = ' . $altpreis . ' WHERE `Brand`="' . $row["Brand"] . '"' ;
-            $conn->query($sql);
-            
-            // Store the  change in the log database
-            $sql = 'INSERT INTO `dasBar`.`geschichte` (`Zeit`, `Preis`, `Brand`, `ID`) VALUES ( NOW(), "' . $preis . '", "' . $row["Brand"] . '" , "' . $row["ID"] . '" );';
-            $conn->query($sql);
-          
-        }
-    } else {
-        echo "0 results";
-    }   
-    
-    $return["data"] = "Preis updated";
-    echo json_encode($return);
-
-    $conn->close();
 }
 
 function construct_marquee($return) {
---
+++ preismeister.php
@@ -0,0 +1,83 @@
+#!/usr/bin/php
+
+<?php
+include 'cred.php';
+
+declare(ticks = 1);
+
+pcntl_signal(SIGTERM, "signal_handler");
+pcntl_signal(SIGINT, "signal_handler");
+
+function signal_handler($signal) {
+    switch($signal) {
+    case SIGINT:
+        print "Caught SIGINT\n";
+        global $conn;
+        $conn->close();
+        exit;
+    case SIGTERM:
+        print "Caught SIGTERM\n";
+        global $conn;
+        $conn->close();
+        exit;        
+    case SIGKILL:
+        print "Caught SIGKILL\n";
+        global $conn;
+        $conn->close();
+        exit;        
+    }
+}
+        
+
+// To increment the Preis on all bier sorts.
+// They will increase the Preis varje time sidan ist refreshed.
+// Run a new loop, just for the sake of it ...
+global $servername, $username, $password, $dbname;
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}     
+
+while (true) {
+    $sql = "SELECT * FROM `komponent`";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $preis = $row["Preis"];
+            $orig  = $row["Originalpreis"];
+            $mini  = $row["Minimipreis"];
+            $altpreis = $preis;
+            
+            // Algorithm to set the Preis - be creative...
+            $preis = $preis - ($preis-$mini)*0.05 + 0.2*rand(0, 2); 
+            
+            // We shouldn't loose money
+            if ($preis < $mini) {
+                $preis = $mini;
+            }	  
+            
+            echo "Changing price of " . $row["Prettyprint"] . " from " . $altpreis . " to " . $preis . "\n";
+            
+            // Set the new Preis and update the old Preis.
+            $sql = 'UPDATE `komponent` SET `Preis` = ' . $preis . ' WHERE `Brand`="' . $row["Brand"] . '"' ;
+            $conn->query($sql);
+            $sql = 'UPDATE `komponent` SET `Altpreis` = ' . $altpreis . ' WHERE `Brand`="' . $row["Brand"] . '"' ;
+            $conn->query($sql);
+            
+            // Store the  change in the log database
+            $sql = 'INSERT INTO `dasBar`.`geschichte` (`Zeit`, `Preis`, `Brand`, `ID`) VALUES ( NOW(), "' . $preis . '", "' . $row["Brand"] . '" , "' . $row["ID"] . '" );';
+            $conn->query($sql);
+            
+        }
+    } else {
+        echo "0 results";
+    }   
+    echo "===================================\n";
+    sleep(60);
+
+}
+$conn->close();
+
+?>
+
+
